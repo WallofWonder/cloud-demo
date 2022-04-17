a7ada89bd1a0c2e348e7cf977795c081d614e098
ZYF <m13110683916@163.com>
2022-04-17 20:50:23 +0800

    Eureka服务发现，负载均衡

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,6 +3,7 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +16,7 @@ public class OrderApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-
 @Service
 public class OrderService {
 
@@ -22,7 +20,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2.利用RestTemplate发起http请求
-        String url = "http://localhost:8081/user/" + order.getUserId();
+        String url = "http://userservice/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
         // 4.返回
