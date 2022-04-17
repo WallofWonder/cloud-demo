17f9f24944a6189cf93a22c664e2d073f063c66b
ZYF <m13110683916@163.com>
2022-04-17 16:26:38 +0800

    init

---
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package cn.itcast.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
