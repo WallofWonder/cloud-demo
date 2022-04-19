93dd94899816a6175ac97c79b8fb55dcbca354b9
ZYF <m13110683916@163.com>
2022-04-19 16:36:42 +0800

    nacos实现配置热更新

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,10 +1,10 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +21,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Value("${pattern.dateformat}")
-    private String dateformat;
+    @Autowired
+    private PatternProperties patternProperties;
 
     @GetMapping("now")
-    public String now(){
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
     }
 
     /**
