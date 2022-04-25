4f84cc325a68936db9630eaa70a9ae214df287d4
ZYF <m13110683916@163.com>
2022-04-25 09:47:13 +0800

    Feign 自定义日志配置

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package cn.itcast.order;
 
+import cn.itcast.order.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = DefaultFeignConfiguration.class)
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
---
+++ order-service/src/main/java/cn/itcast/order/config/DefaultFeignConfiguration.java
@@ -0,0 +1,11 @@
+package cn.itcast.order.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+public class DefaultFeignConfiguration  {
+    @Bean
+    public Logger.Level feignLogLevel(){
+        return Logger.Level.BASIC; // 日志级别为BASIC
+    }
+}
