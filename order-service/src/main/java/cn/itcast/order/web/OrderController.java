8324bb2d332ab5472b72982c3948910fca4d2550
ZYF <m13110683916@163.com>
2022-05-04 15:44:48 +0800

    流控-关联模式

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -20,4 +20,14 @@ public class OrderController {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
     }
+
+    @GetMapping("/query")
+    public String queryOrder() {
+        return "查询订单成功";
+    }
+
+    @GetMapping("/update")
+    public String updateOrder() {
+        return "更新订单成功";
+    }
 }
